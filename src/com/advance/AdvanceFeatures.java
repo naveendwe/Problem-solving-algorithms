b7fe58c81970e6816efa59d02a2a667438a8b773
naveen2000 <naveendwedi7224@gmail.com>
2025-09-18 22:54:06 +0530

    updated java 11 features

---
+++ src/com/advance/AdvanceFeatures.java
@@ -0,0 +1,42 @@
+package com.advance;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AdvanceFeatures {
+    public static void main(String[] args) {
+        //🟢 Java 11 Features
+        String s = "   Hello   ";
+        // ✅ isBlank
+        System.out.println("   ".isBlank());
+        System.out.println(s.strip()); // "Hello"
+        System.out.println(s.stripLeading()); // "Hello  ";
+        System.out.println(s.stripTrailing()); // "  Hello";
+
+        System.out.println(s.repeat(3)); // "Hello Hello Hello"
+
+        String multiline = "Hello\nJava\nand\nC++";
+        multiline.lines().forEach(System.out::println);
+        System.out.println(multiline);
+
+    }
+    void A(){
+        List<Integer> list = List.of(1,3,4,5,6);
+        Integer[] arr = list.toArray(Integer[]::new);
+        Optional<?> opt = Optional.empty();
+        System.out.println(opt.isEmpty());
+    }
+}
+sealed interface Car permits Engine{
+    void start();
+}
+non-sealed class Engine implements Car {
+
+    @Override
+    public void start() {
+
+    }
+}
+class Diesel extends Engine{
+
+}
